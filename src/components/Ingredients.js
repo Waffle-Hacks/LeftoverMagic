a425a9f9c8c723e37ebdf3de1c5f7a7d672de1b1
Sabrina Zheng <szheng@Sabrinas-MBP.home>
2023-06-24 19:52:49 -0400

    Updated ingredient page header

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -9,8 +9,8 @@ const Ingredients = () => {
     const btnTheme = {
         basic: {
             borderRadius: '75px',
-            margin: '15px 2.5%',
-            padding: '0px 25px',
+            margin: '20px 5px',
+            padding: '10px 10px',
             textTransform: 'capitalize',
             '&.MuiButton-root:hover':{
                 backgroundColor: 'var(--primary-hover-color)',
@@ -28,15 +28,17 @@ const Ingredients = () => {
 
     return(
         <div className='subContainer'>
-            <div className='container'>
-                 <Typography variant='h5' fontWeight="bold" sx={fontTheme}>Manage Ingredients</Typography>
-                 <div className='container'>
-                    <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Add Ingredients</Button>
-                    <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Undo</Button>
-                    <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Redo</Button>
-                    <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Sort By</Button>
-                 </div>
-                </div>
+            <div className='ingredientHeader'>
+                    <div className='ingredientTitle'>
+                        <Typography variant='h5' fontWeight="bold" sx={fontTheme}>Manage Ingredients</Typography>
+                    </div>
+                    <div className='headerBtn'>
+                        <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Add Ingredients</Button>
+                        <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Undo</Button>
+                        <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Redo</Button>
+                        <Button variant='contained' sx={[ btnTheme.basic, btnTheme.contained ]}>Sort By</Button>
+                    </div>
+            </div>
         </div>
     )
 }
