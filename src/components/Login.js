bbfe6ba5fd11225a773f55c2a30f33ce206561f7
Sabrina Zheng <szheng@Sabrinas-MacBook-Pro.local>
2023-06-24 13:51:45 -0400

    LoginPage Component

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState} from "react"
+
+const Login= () => {
+    return(
+        <div>
+            <h1>Login</h1>
+            <form>
+                <input type="email" placeholder='Email' name="" id="" />
+                <input type="password" placeholder='Password' name="" id="" />
+
+            <input type="submit"/>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
