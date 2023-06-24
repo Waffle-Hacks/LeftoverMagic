459524ef49353cbc0bb21fce8cc212512ef38558
ChoiYingY <ycying1226@gmail.com>
2023-06-24 11:25:39 -0400

    created nav bar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+
+const NavBar = () => {
+    return <AppBar>
+        <Toolbar>
+            {/* <a href='https://chat.openai.com/'>
+                <img src='/assets/logo_horizontal.png' alt='LeftoverMagic logo'></img>
+            </a> */}
+        </Toolbar>
+    </AppBar>
+}
+
+export default NavBar;
