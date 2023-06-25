14bb656dd9d1fce93a1c874c313dacdeebc18357
ChoiYingY <ycying1226@gmail.com>
2023-06-24 21:04:49 -0400

    change home screen based on login state

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,9 +1,14 @@
 import Hero from './Hero';
+import { AuthContext } from '../auth';
+import Ingredients from './Ingredients';
 import DescriptionBox from './DescriptionBox';
 
-import { Typography} from '@mui/material';
+import React, { useContext } from 'react';
+import { Typography } from '@mui/material';
 
 const HomeScreen = () => {
+    const { auth } = useContext(AuthContext);
+    
     const fontTheme = {
         color: 'black',
         margin: '2.5%'
@@ -12,20 +17,31 @@ const HomeScreen = () => {
     const titles = ["Food Invetory", "Recipe Generator"]
     const descriptions = ["Keep track of food items you have at house", "Generate recipes based on your food inventory & preference!"]
 
-    return(
-        <div className='homeContainer'>
-            <div className='subContainer'>
-                <Hero/>
-                <hr id='divider'/>
-                <div className='columnContainer'>
-                    <Typography variant='h3' fontWeight="bold" sx={fontTheme}>Usage</Typography>
-                    <div className='container' id='descriptionContainer'>
-                        {titles.map((item, index) => (
-                            <DescriptionBox key={index} title={item} description={descriptions[index]}/>
-                        ))}
-                    </div>
+    let content = '';
+    if(auth && auth.user && auth.loggedIn){
+        content = <div className='subContainer'>
+            Hello
+            {/* <Ingredients/> */}
+        </div>
+    }
+    else{
+        content = <div className='subContainer'>
+            <Hero/>
+            <hr id='divider'/>
+            <div className='columnContainer'>
+                <Typography variant='h3' fontWeight="bold" sx={fontTheme}>Usage</Typography>
+                <div className='container' id='descriptionContainer'>
+                    {titles.map((item, index) => (
+                        <DescriptionBox key={index} title={item} description={descriptions[index]}/>
+                    ))}
                 </div>
             </div>
+        </div>
+    }
+
+    return(
+        <div className='homeContainer'>
+            {content}
         </div>
     )
 }
